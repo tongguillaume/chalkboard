b6ddafd0056cc7256a016e31636e445eeee37bb1
Guillaume Tong <tong_g@etna-alternance.net>
2021-06-08 10:15:27 +0200

    Create function

--- Limestone/Limestone.php
+++ Limestone/Limestone.php
@@ -3,5 +3,23 @@
 
 class Limestone
 {
+    private function getFolderFiles($path)
+    {
+        $scanned_directory = array_diff(scandir($path), array('..', '.'));
+    }
 
+    private function parseFile($fileContent)
+    {
+
+    }
+
+    private function htmlToXml()
+    {
+
+    }
+
+    private function htmlToYaml()
+    {
+        
+    }
 }
