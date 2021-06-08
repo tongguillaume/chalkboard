d2de20e28cd4d64799a3dbdbf8e136cf0bc1fa7b
Guillaume Tong <tong_g@etna-alternance.net>
2021-06-08 16:27:44 +0200

    continue test

--- Limestone/test.php
+++ Limestone/test.php
@@ -6,25 +6,8 @@ include 'Limestone.php';
 
 
 $test = new Limestone();
-$test->getFolderFiles('../templates');
+$mama = $test->getFolderFiles('../templates');
 
-$testParse = $test->parseFile("<html>
-<title>Ignored Title</title>
-<body>
-  <h1>Hello, World!</h1>
-
-  <p>This is some e-mail content.
-  Even though it has whitespace and newlines, the e-mail converter
-  will handle it correctly.
-
-  <p>Even mismatched tags.</p>
-
-  <div>A div</div>
-  <div>Another div</div>
-  <div>A div<div>within a div</div></div>
-
-  <a href='http://foo.com'>A link</a>
-
-</body>
-</html>");
-echo $testParse;
+foreach ($mama as $file) {
+    $test->parseFile($file);
+}
